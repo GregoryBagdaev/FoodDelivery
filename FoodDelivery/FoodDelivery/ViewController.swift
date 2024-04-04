d8131fc1b24386cb0759f2a1352a5e66325f6e6e
Gregory <Greg@MacBook-Air-GB.local>
2024-04-04 14:55:39 +0300

    Created base project, removed storyboard references

---
+++ FoodDelivery/FoodDelivery/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Gregory on 4.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+
+    }
+
+
+}
+
