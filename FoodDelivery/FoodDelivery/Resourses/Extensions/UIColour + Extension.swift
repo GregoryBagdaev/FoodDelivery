ac4c34a6bed2cf6365af8b94609dbaf4b8ecaf3a
Gregory <Greg@MacBook-Air-GB.local>
2024-04-05 12:12:55 +0300

    Added new base colors

---
+++ FoodDelivery/FoodDelivery/Resourses/Extensions/UIColour + Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIColour + Extension.swift
+//  FoodDelivery
+//
+//  Created by Gregory on 5.04.2024.
+//
+
+import UIKit
+
+extension UIColor {
+    func hex(_ rgbValue: UInt64) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0xFF0000) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0xFF0000) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
